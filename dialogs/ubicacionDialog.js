2d86e30bfc32440fa825ebc21a710cb1fc6ccbb5
Esanchezl <42590200+esanchezlMBT@users.noreply.github.com>
2019-08-30 12:51:49 -0500

    Release 1.4
    ActivityPrompt

--- dialogs/ubicacionDialog.js
+++ dialogs/ubicacionDialog.js
@@ -1,14 +1,16 @@
-const { ComponentDialog, WaterfallDialog, TextPrompt, DateTimePrompt } = require('botbuilder-dialogs');
+const { ComponentDialog, WaterfallDialog, TextPrompt, ActivityPrompt } = require('botbuilder-dialogs');
 
 const UBICACION_DIALOG = "UBICACION_DIALOG";
 const TEXT_PROMPT = "TEXT_PROMPT";
 const WATERFALL_DIALOG = 'WATERFALL_DIALOG';
+const ACTIVITY_PROMPT = 'ACTIVITY_PROMPT';
 
 class UbicacionDialog extends ComponentDialog{
     constructor(){
         super(UBICACION_DIALOG);
 
         this.addDialog(new TextPrompt(TEXT_PROMPT));
+        this.addDialog(new ActivityPrompt(ACTIVITY_PROMPT));
         this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
             this.ubicacionStep.bind(this),
             this.guardarStep.bind(this)
@@ -23,7 +25,7 @@ class UbicacionDialog extends ComponentDialog{
         
         // await step.context.sendActivity('Por favor comparte tu ubicación');
         return await step.prompt(
-            TEXT_PROMPT, {
+            ACTIVITY_PROMPT, {
                 prompt: 'Comparte tu ubicación'
             }
             );
