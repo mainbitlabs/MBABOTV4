44a4b1312038e4a12958d8e66b6e60166ac13b1a
Esanchezl <42590200+esanchezlMBT@users.noreply.github.com>
2019-09-27 11:08:23 -0500

    Issue 1.3
    Handling error.

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ adapter.onTurnError = async (context, error) => {
     console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
     await context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    await conversationState.load(context);
+    await conversationState.clear(context);
+    // Save state changes.
+    await conversationState.saveChanges(context);
 };
 
 // Define the state store for your bot.
